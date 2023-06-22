c60933b65bcd2a0e26d7e41cc83b2b79a4bbcb27
mr_sd_jack_003 <barson0habra@gmail.com>
2023-06-23 03:24:46 +0530

    user not connected each other "not fixed"

--- public/script.js
+++ public/script.js
@@ -4,25 +4,45 @@ console.log(videoGrid);
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+});
 
-let myVideoStream
+let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+
+    socket.on('user-connected', (userId) =>{
+        connecToNewUser(userId, stream);
+     })
 })
 
-socket.emit('join-room', ROOM_ID);
-
-socket.on('user-connected', () =>{
-    connecToNewUser();
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
 })
 
 
-const connecToNewUser = () =>{
-    console.log('new user');
+const connecToNewUser = (userId, stream) =>{
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
 }
 
 
